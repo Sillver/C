d05c76eeab0cafba0ca04971338707fecfb49e43
Sillver <obin225@gmail.com>
2013-11-10 12:02:01 +0100

    Update and rename README.md to Zadanie 6.c

---
+++ Zadanie 6.c
@@ -0,0 +1,19 @@
+C
+
+#include <stdio.h>
+#include <math.h>
+int main() {
+
+double x;
+double z;
+        printf("wpisz liczbe rzeczywista \n");
+        scanf("%lf", &x);
+                while (x < 0){
+                        printf ("%lf to nie jest liczba rzeczywista \n sprobuj jeszcze raz \n", x);
+                        scanf("%lf",& x);
+        }
+        z = sqrt(x);
+        printf("Pierwiastek kwadratowy z %lf to %lf\n", x, z);
+return 0;
+}
+
